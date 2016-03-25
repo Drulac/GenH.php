f2301eeb2cb9a97d0c5effaa99589fb69b7d8659
Drulac <dev.drulac@gmail.com>
2016-03-25 19:34:23 +0100

    Update sample.php

--- sample.php
+++ sample.php
@@ -5,18 +5,11 @@ use PhpGenHTML as V;
 
 $view = New V\View;
 echo $view->start();
-echo $view->view(array(
-	New V\Head(array(
-		New V\Title('View all statistiques'),
-		New V\Script('', array('src' => 'Chart.js')),
-		New V\Meta(array('name' => 'viewport', 'content' => 'initial-scale = 1, user-scalable = no'))
-	))
-));
 echo $view->bodyStart();
 echo $view->view(array(
 	New V\Div(array(
-		New V\Canvas('', array('id' => 'canvas', 'height' => 450, 'width' => 450)),
-		New V\Canvas('', array('id' => 'chart-area', 'height' => 300, 'width' => 300))
+		New V\H1('Sample'),
+		New V\H2('Test', array('id' => 'htwo', 'class' => 'test'))
 	), array('style' => 'width:100%'))
 ));
 echo $view->end();
